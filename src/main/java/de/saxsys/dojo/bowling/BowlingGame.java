c20f76c49cbb3c78202022d84fa9f58e45df1131
Stefan Bley <stefan.bley@saxsys.de>
2014-03-21 15:26:31 +0100

    as of end of dojo

--- src/main/java/de/saxsys/dojo/bowling/BowlingGame.java
+++ src/main/java/de/saxsys/dojo/bowling/BowlingGame.java
@@ -1,6 +1,47 @@
 package de.saxsys.dojo.bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
 
-	// TODO
+	public class Frame {
+		private Integer firstRoll;
+		private Integer secondRoll;
+
+	}
+
+	private List<Integer> rolls = new ArrayList<>();
+	private List<Frame> frames = new ArrayList<>();
+
+	public int score() {
+		int score = 0;
+		for (int i = 0; i < rolls.size(); i++) {
+			score += rolls.get(i);
+			if (wasStrikeOneRollAgo(i)) {
+				score += (rolls.get(i));
+			}
+			if (wasStrikeTwoRollsAgo(i)) {
+				score += (rolls.get(i));
+			}
+		}
+		return score;
+	}
+
+	public void roll(int pins) {
+		rolls.add(pins);
+		if (frames.isEmpty()) {
+			frames.add(new Frame());
+		}
+		frames.get(frames.size() - 1).firstRoll = pins;
+	}
+
+	private boolean wasStrikeTwoRollsAgo(int i) {
+		return i > 1 && rolls.get(i - 2) == 10;
+	}
+
+	private boolean wasStrikeOneRollAgo(int i) {
+		return i > 0 && rolls.get(i - 1) == 10;
+	}
+
 }
--- src/test/java/de/saxsys/dojo/bowling/BowlingGameTest.java
+++ src/test/java/de/saxsys/dojo/bowling/BowlingGameTest.java
@@ -1,6 +1,75 @@
 package de.saxsys.dojo.bowling;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class BowlingGameTest {
 
-	// TODO
+	private BowlingGame game;
+
+	@Before
+	public void setUp() {
+		game = new BowlingGame();
+	}
+
+	@Test
+	public void noRollShouldGiveScoreZero() throws Exception {
+		assertThat(game.score(), is(0));
+	}
+
+	@Test
+	public void rollZeroShouldGiveScoreZero() throws Exception {
+		game.roll(0);
+		assertThat(game.score(), is(0));
+	}
+
+	@Test
+	public void rollOneShouldGiveScoreOne() throws Exception {
+		game.roll(1);
+		assertThat(game.score(), is(1));
+	}
+
+	@Test
+	public void rollTwoAndThreeShouldGiveFive() throws Exception {
+		game.roll(2);
+		game.roll(3);
+		assertThat(game.score(), is(5));
+	}
+
+	@Test
+	public void strikeShouldDoubleNextFrame() throws Exception {
+		game.roll(10);
+		game.roll(2);
+		game.roll(5);
+		assertThat(game.score(), is(24));
+	}
+
+	@Test
+	public void threeStrikesShouldGive60() throws Exception {
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		assertThat(game.score(), is(60));
+	}
+
+	@Test
+	public void fourRollsShouldGive75() throws Exception {
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		game.roll(5);
+		assertThat(game.score(), is(75));
+	}
+
+	// @Test
+	// public void oneSpareAndFiveShouldGive20() throws Exception {
+	// game.roll(6);
+	// game.roll(4);
+	// game.roll(5);
+	// assertThat(game.score(), is(20));
+	// }
+
 }
